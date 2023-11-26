60f36f9f9ac94b4edd5874ecee6a24fd943547ff
Anton Sadagov <anton.sadagov@gmail.com>
2023-11-26 15:54:51 +0300

    v1_precommit

--- form_scrapper/tests/test_data.py
+++ form_scrapper/tests/test_data.py
@@ -1,8 +1,7 @@
 TEST_TYPES = {
-    'aaa@aaa.com': 'email',
-    '+7 999 999 99 99': 'phone',
-    'name': 'text',
-    1: type(1),
-    '25.03.1992': 'date',
-    '1992-03-25': 'date',
+    "aaa@aaa.com": "email",
+    "+7 999 999 99 99": "phone",
+    "name": "text",
+    "25.03.1992": "date",
+    "1992-03-25": "date",
 }
